3a0391c6efd998f0c2b006af369a3ffa7d126d18
Suthima Sontead <suthima.so@ku.th>
2020-02-13 21:45:00 +0700

    printf()

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define for "gdsfed"
+float e;
+char r[] = "kong";
+int main()
+{
+    printf("%-10s\n", for);
+    // printf("Hello World!")
+    int a = 400;
+    char b = 'a';
+    char c = 97;
+    float f = 0.1245;
+    double d = 20.18888;
+    // printf("%c %d %c %d", b, b, c, c);
+    // int ans1 = a+b;
+    // float ans2 = f+b;
+    float ans3 = 11%15;
+    printf("%f", ans3);
+}
